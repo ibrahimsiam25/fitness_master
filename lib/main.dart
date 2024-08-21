b68bc2a119de53a69aa2e93943eb51108d9e14ce
ibrahimsiam25 <ebrahimfathy2822@gmail.com>
2024-08-21 13:50:21 +0300

    Add Flutter ScreenUtil for Responsive Design and Initial Setup
    This commit introduces the Flutter ScreenUtil package to the project, enhancing the app's responsiveness across different screen sizes. The main.dart file has been updated to include ScreenUtil initialization, setting a design size, enabling text adaptation, and split-screen mode. Additionally, the SystemChrome settings have been adjusted to improve the app's visual consistency, particularly with the status bar icons and color.
    
    The pubspec.yaml file has been modified to include the flutter_screenutil dependency, ensuring version 5.9.3 is used. This addition allows for more efficient management of UI elements across various devices, improving the overall user experience.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async {
+  await ScreenUtil.ensureScreenSize();
   runApp(const MyApp());
 }
- class MyApp extends StatelessWidget {
+
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        SystemChrome.setSystemUIOverlayStyle(
+          const SystemUiOverlayStyle(
+            statusBarIconBrightness: Brightness.light,
+            statusBarColor: Colors.transparent,
+          ),
+        );
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: child,
+        );
+      },
+    );
   }
-}     
+}
