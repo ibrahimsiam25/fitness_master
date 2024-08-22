925841f5340518d283e167a7a24229d52c7fa756
ibrahimsiam25 <ebrahimfathy2822@gmail.com>
2024-08-22 18:26:00 +0300

    refactor

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,12 +1,9 @@
-import 'package:fitness_master/features/home/presentation/views/widgets/todays_time_counter.dart';
 import 'package:flutter/material.dart';
 import 'package:fitness_master/core/constants/const.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-<<<<<<< HEAD
-import 'package:fitness_master/features/home/presentation/views/widgets/Todays_time_counter.dart';
-=======
->>>>>>> 7598dbc15301407b34e2f75ce657f03b3c93033e
+import 'package:fitness_master/features/home/presentation/views/widgets/todays_time_counter.dart';
 import 'package:fitness_master/features/home/presentation/views/widgets/custom_drob_contianer.dart';
+
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -24,13 +21,6 @@ class HomeViewBody extends StatelessWidget {
           SizedBox(
             height: 24.h,
           ),
-<<<<<<< HEAD
-           const TodaysTimeCounter()
-=======
-          const TodaysTimeCounter(),
->>>>>>> 7598dbc15301407b34e2f75ce657f03b3c93033e
-        ],
-      ),
-    );
+           const TodaysTimeCounter()]));
   }
 }
