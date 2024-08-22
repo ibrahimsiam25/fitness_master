95871fc6880adf849b778395388635e04d83a744
ibrahimsiam25 <ebrahimfathy2822@gmail.com>
2024-08-22 16:12:14 +0300

    add Today's time counter

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import '../../../../../core/utils/app_styles.dart';
 import 'package:fitness_master/core/constants/const.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:fitness_master/core/constants/app_color.dart';
+import 'package:fitness_master/features/home/presentation/views/widgets/Todays_time_counter.dart';
 import 'package:fitness_master/features/home/presentation/views/widgets/custom_drob_contianer.dart';
 
 
@@ -18,18 +17,11 @@ class HomeViewBody extends StatelessWidget {
           SizedBox(
             height: 24.h,
           ),
-          CustomDrobContainer(),
-          Container(
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(14.r),
-                color: AppColor.black),
-                child: Row(
-                  children: [
-                    Text("Today's time counter",style: AppStyles.body2,),
-                    
-                  ],
-                ),
-          )
+       const   CustomDrobContainer(),
+              SizedBox(
+            height: 24.h,
+          ),
+        const  TodaysTimeCounter()
         ],
       ),
     );
