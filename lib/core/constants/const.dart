d8e1e4fa4b631795b3831cf6f193f2b831ef5bef
itiswd <itiswdev97@gmail.com>
2024-08-22 23:19:04 +0300

    Update constants file with improved formatting
    Added a newline before the `premiumList` declaration to improve readability and consistency with other constants. This change aligns with the project's coding standards, making the codebase more maintainable and easier to understand.

--- lib/core/constants/const.dart
+++ lib/core/constants/const.dart
@@ -3,6 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 const String kIsOnBoardingView = 'isOnBoardingView';
 const double kHorizontalPadding = 27.0;
 const kVerticalPadding = 16;
+
 List premiumList = [
   'access to all exercises and articles',
   'the ability to set a goal to exercise\nmore than 4 days a week',
@@ -32,4 +33,3 @@ List settingsTitles = [
   'Support',
   'Restore purchases',
 ];
-
