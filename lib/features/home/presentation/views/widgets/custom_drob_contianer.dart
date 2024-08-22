1c3cd88da15f23b07c7fb7925146317769b6133c
ibrahimsiam25 <ebrahimfathy2822@gmail.com>
2024-08-22 17:40:42 +0300

    Add calendar to home view

--- lib/features/home/presentation/views/widgets/custom_drob_contianer.dart
+++ lib/features/home/presentation/views/widgets/custom_drob_contianer.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:svg_flutter/svg_flutter.dart';
 import '../../../../../core/utils/app_styles.dart';
+import 'package:table_calendar/table_calendar.dart';
 import '../../../../../core/constants/app_color.dart';
 import '../../../../../core/constants/app_assets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class CustomDrobContainer extends StatelessWidget {
+class CustomDrobContainer extends StatefulWidget {
   const CustomDrobContainer({
     super.key,
   });
 
+  @override
+  State<CustomDrobContainer> createState() => _CustomDrobContainerState();
+}
+
+class _CustomDrobContainerState extends State<CustomDrobContainer> {
+  bool isDrobed = false;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -18,24 +25,46 @@ class CustomDrobContainer extends StatelessWidget {
           color: AppColor.black,
         ),
         padding: EdgeInsets.all(12.r),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Column(
           children: [
-            SvgPicture.asset(AppAssets.fireIcon),
-            Column(
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text(
-                  "Weeks streak",
-                  style: AppStyles.body2,
+                SvgPicture.asset(AppAssets.fireIcon),
+                Column(
+                  children: [
+                    Text(
+                      "Weeks streak",
+                      style: AppStyles.body2,
+                    ),
+                    Text(
+                      "Keep going!",
+                      style: AppStyles.body1,
+                    ),
+                  ],
                 ),
-                Text(
-                  "Keep going!",
-                  style: AppStyles.body1,
-                ),
+                IconButton(
+                    onPressed: () {
+                      setState(() {
+                        isDrobed = !isDrobed;
+                      });
+                    },
+                    icon: SvgPicture.asset(AppAssets.arrowDown))
               ],
             ),
-            IconButton(
-                onPressed: () {}, icon: SvgPicture.asset(AppAssets.arrowDown))
+            Visibility(
+              visible: isDrobed,
+              child: Container(
+                color: Colors.black,
+                child:TableCalendar(
+                  daysOfWeekVisible: false,
+              
+  firstDay: DateTime.utc(2010, 10, 16),
+  lastDay: DateTime.utc(2030, 3, 14),
+  focusedDay: DateTime.now(),
+),
+              ),
+            )
           ],
         ));
   }
