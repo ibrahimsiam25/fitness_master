5d90bab9d2a38375005874c7387ec1d50524c7a5
ibrahimsiam25 <ebrahimfathy2822@gmail.com>
2024-08-22 14:40:13 +0300

    create custom app bar  to home view

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:svg_flutter/svg_flutter.dart';
+import '../../../../core/widgets/custom_app_bar.dart';
+import 'package:fitness_master/core/utils/app_styles.dart';
+import 'package:fitness_master/core/constants/app_assets.dart';
 import 'package:fitness_master/features/home/presentation/views/widgets/home_view_body.dart';
+
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: const HomeViewBody());
+    return Scaffold(
+        appBar: cusyomAppBar(
+            title: "Home",
+            icon: SvgPicture.asset(AppAssets.editIcon),
+            onPressed: () {}),
+        body: HomeViewBody());
   }
+
+
 }
