796a0a577e5a70d6a130e417fa31d2dbbb2cc846
itiswd <itiswdev97@gmail.com>
2024-08-23 15:31:37 +0300

    Add Progress View Body Widget for Fitness App
    This commit introduces the `ProgressViewBody` widget, a crucial component of the fitness app's progress tracking feature. The widget is designed to display progress data for various body parts across different months, utilizing a structured map (`progressMap`) to organize and retrieve this data.
    
    The `ProgressViewBody` widget includes:
    - A header section with the title "Progress" and an edit icon for user interaction.
    - A scrollable list view that dynamically generates `ProgressCategoryCard` widgets based on the data in `progressMap`.
    - Integration with `flutter_screenutil` for responsive design and `svg_flutter` for rendering SVG icons.
    
    This addition enhances the app's functionality by providing a clear and interactive interface for users to monitor their fitness progress over time.

---
+++ lib/features/progress/presentation/views/widgets/progress_view_body.dart
@@ -0,0 +1,67 @@
+import 'package:fitness_master/core/utils/app_styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:fitness_master/core/constants/app_assets.dart';
+import 'package:svg_flutter/svg.dart';
+
+final Map<String, Map<String, String>> progressMap = {
+  "Biceps": {
+    "April": AppAssets.progress,
+    "May": AppAssets.progress,
+    "June": AppAssets.progress,
+    "August": AppAssets.progress,
+  },
+  "Abdomen": {
+    "April": AppAssets.progress,
+    "May": AppAssets.progress,
+    "June": AppAssets.progress,
+    "August": AppAssets.progress,
+  },
+};
+
+class ProgressViewBody extends StatelessWidget {
+  const ProgressViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 54.h,
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0.w),
+          child: Row(
+            children: [
+              Text(
+                'Progress',
+                style: AppStyles.header1,
+              ),
+              const Spacer(),
+              InkWell(
+                onTap: () {},
+                child: SvgPicture.asset(
+                  AppAssets.editIcon,
+                ),
+              )
+            ],
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            padding: EdgeInsets.zero,
+            itemCount: progressMap.length,
+            itemBuilder: (context, index) {
+              final category = progressMap.keys.elementAt(index);
+              final progressData = progressMap[category]!;
+              return ProgressCategoryCard(
+                category: category,
+                progressData: progressData,
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
