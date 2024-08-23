bd03300b7db20b4b3fb580fe6a37f9ef12600afd
itiswd <itiswdev97@gmail.com>
2024-08-23 15:30:15 +0300

    Refactor ProgressView and ProgressViewBody for improved structure and UI
    - Removed redundant imports in ProgressView.
    - Simplified ProgressView by removing custom app bar configuration directly within the widget.
    - Enhanced ProgressViewBody with a more structured layout:
      - Added a header section with a title and an edit icon for better user interaction.
      - Wrapped the ListView within an Expanded widget to ensure proper rendering and scrolling behavior.
      - Utilized SvgPicture for rendering SVG assets, enhancing visual consistency and performance.
    - Integrated AppStyles for text styling, ensuring a cohesive design language across the application.

--- lib/features/progress/presentation/views/progress_view.dart
+++ lib/features/progress/presentation/views/progress_view.dart
@@ -1,24 +1,13 @@
 import 'package:flutter/material.dart';
 import 'widgets/progress_view_body.dart';
-import '../../../../core/widgets/custom_app_bar.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:fitness_master/core/constants/app_color.dart';
 
 class ProgressView extends StatelessWidget {
   const ProgressView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: customAppBar(
-          title: "Progress",
-          icon: Icon(
-            Icons.add,
-            color: AppColor.white,
-            size: 24.r,
-          ),
-          onPressed: () {}),
-      body: const ProgressViewBody(),
+    return const Scaffold(
+      body: ProgressViewBody(),
     );
   }
 }
--- lib/features/progress/presentation/views/widgets/progress_view_body.dart
+++ lib/features/progress/presentation/views/widgets/progress_view_body.dart
@@ -1,7 +1,9 @@
+import 'package:fitness_master/core/utils/app_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fitness_master/core/constants/app_assets.dart';
 import 'package:fitness_master/features/progress/presentation/views/widgets/progress_category_card.dart';
+import 'package:svg_flutter/svg.dart';
 
 final Map<String, Map<String, String>> progressMap = {
   "Biceps": {
@@ -23,19 +25,44 @@ class ProgressViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: 14.h),
-      child: ListView.builder(
-        itemCount: progressMap.length,
-        itemBuilder: (context, index) {
-          final category = progressMap.keys.elementAt(index);
-          final progressData = progressMap[category]!;
-          return ProgressCategoryCard(
-            category: category,
-            progressData: progressData,
-          );
-        },
-      ),
+    return Column(
+      children: [
+        SizedBox(
+          height: 54.h,
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0.w),
+          child: Row(
+            children: [
+              Text(
+                'Progress',
+                style: AppStyles.header1,
+              ),
+              const Spacer(),
+              InkWell(
+                onTap: () {},
+                child: SvgPicture.asset(
+                  AppAssets.editIcon,
+                ),
+              )
+            ],
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            padding: EdgeInsets.zero,
+            itemCount: progressMap.length,
+            itemBuilder: (context, index) {
+              final category = progressMap.keys.elementAt(index);
+              final progressData = progressMap[category]!;
+              return ProgressCategoryCard(
+                category: category,
+                progressData: progressData,
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
